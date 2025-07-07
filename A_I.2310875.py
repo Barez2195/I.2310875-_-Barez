0d5bd68ae61f2764951da8719ee8e9ee7c739bc6
Barez2195 <mbarezsapadoss@gmail.com>
2025-07-07 18:36:44 +0700

    Add files via upload

---
+++ A_I.2310875.py
@@ -0,0 +1,4 @@
+nama = input("MUHAMMAD BAREZ SAPADO SIREGAR: ")
+universitas = input("Universitas Djuanda: ")
+nim = input("I.2310875: ")
+print(f"\nPerkenalkan, nama saya {Barez}, saya kuliah di {Djuanda} dengan NIM {I.2310875}.")
